d9ac6222dc9d69a27d3054301901d35941d9e00a
patriciaasiimire <zoeqwiggly@gmail.com>
2024-10-16 16:21:07 +0300

    fixed the diet icons to fit the boxes well

--- lib/models/diet_model.dart
+++ lib/models/diet_model.dart
@@ -22,32 +22,41 @@ class DietModel {
     List<DietModel> diets = [];
 
     diets.add(DietModel(
-        name: 'Honey Pancake',
-        iconPath: 'assets/icons/pancakes-svgrepo-com (1).svg',
-        level: 'Easy',
-        duration: '30mins',
-        calorie: '180kCal',
-        viewIsSelected: true,
-        boxColor: const Color.fromARGB(255, 235, 175, 79)));
-    
-    diets.add(DietModel(
-        name: 'Delicious Chicken',
+        name: 'Tasty Chicken',
         iconPath: 'assets/icons/chicken-svgrepo-com.svg',
         level: 'Easy',
         duration: '30mins',
         calorie: '180kCal',
         viewIsSelected: true,
-        boxColor: const Color.fromARGB(255, 79, 235, 222)));
-    
+        boxColor: const Color.fromARGB(255, 235, 175, 79)));
+        
     diets.add(DietModel(
-        name: 'Tasty Pizza',
-        iconPath: 'assets/icons/pizza.svg',
+        name: 'Beef stew',
+        iconPath: 'assets/icons/pot-of-food-svgrepo-com.svg',
         level: 'Easy',
-        duration: '60mins',
+        duration: '30mins',
         calorie: '180kCal',
         viewIsSelected: true,
-        boxColor: const Color.fromARGB(255, 232, 157, 169)));
-    
+        boxColor: const Color.fromARGB(255, 231, 135, 135)));
+        
+    diets.add(DietModel(
+        name: 'Delicious Pie',
+        iconPath: 'assets/icons/pie-food-and-restaurant-svgrepo-com.svg',
+        level: 'Easy',
+        duration: '30mins',
+        calorie: '180kCal',
+        viewIsSelected: true,
+        boxColor: const Color.fromARGB(255, 186, 225, 177)));
+        
+    diets.add(DietModel(
+        name: 'Tasty Hamburger',
+        iconPath: 'assets/icons/hamburger-burger-svgrepo-com.svg',
+        level: 'Easy',
+        duration: '30mins',
+        calorie: '180kCal',
+        viewIsSelected: true,
+        boxColor: const Color.fromARGB(255, 195, 223, 226)));
+            
     return diets;
   }
 }
